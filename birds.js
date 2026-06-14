7c0a747c51d16373532cbc092e9ccdf444d13d04
Ashish Bharti <ashishbhartiid@gmail.com>
2026-06-14 18:55:28 +0530

    update

---
+++ birds.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', () => {
+      VANTA.BIRDS({
+        el: "#digi",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        backgroundColor: 0x0c5b21,
+        colorMode: "variance",
+        birdSize: 0.50
+      })
+    })
